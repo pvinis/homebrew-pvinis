7ad67c095fdcfe2561a5ab89fab4527529a3ce8a
Pavlos Vinieratos <pvinis@gmail.com>
2017-09-23 16:07:42 +0200

    update colortools

--- Formula/colortools.rb
+++ Formula/colortools.rb
@@ -1,15 +1,14 @@
 class Colortools < Formula
   desc "List of shell scripts to print system colors and true colors."
   homepage "https://github.com/pvinis/colortools"
-  url "https://github.com/pvinis/colortools/releases/download/v0.0.1/colortools-0.0.1.zip"
-  sha256 "733f68ebbf942b8ee875f212804a416941962c8845e1cec5127056bd7d31a237"
+  url "https://github.com/pvinis/colortools/releases/download/v1.1/colortools-1.1.zip"
+  sha256 "8b8220fa6e37890930c72830ae04ccb3bf35125f8f7365e40351f4772ab13789"
 
   def install
     bin.install Dir["./*"]
   end
 
   test do
-    assert_match "TRUECOLOR", shell_output("#{bin}/ct-true")
+    assert_match "TRUECOLOR", shell_output("#{bin}/colortools-true")
   end
 end
-
