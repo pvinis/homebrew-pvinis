e5fc347e8c84aa735ebfcf5d5ad9b987322cea7c
Pavlos Vinieratos <pvinis@gmail.com>
2019-06-28 13:19:18 +0200

    Update pomo.rb

--- Formula/pomo.rb
+++ Formula/pomo.rb
@@ -1,14 +1,18 @@
+version = "0.7.1"
+name = "pomo-#{version}-darwin-amd64"
+
 class Pomo < Formula
   desc "Pomodoro CLI"
   homepage "https://kevinschoon.github.io/pomo"
-  url "https://github.com/kevinschoon/pomo/releases/download/0.7.1/pomo-0.7.1-darwin-amd64"
+  url "https://github.com/kevinschoon/pomo/releases/download/#{version}/#{name}"
   sha256 "506319c7f73ada394473b7be07d2eade16374a2682c497feea5f5d51ccf10b26"
 
   def install
+    mv "#{name}" pomo
     bin.install Dir["./*"]
   end
 
   test do
-    assert_match "ok", shell_output("#{bin}/pomo")
+    assert_match "#{version}", shell_output("#{bin}/pomo -v")
   end
 end
