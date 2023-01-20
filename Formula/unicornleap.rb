2c106b1ea2f9177f61f9abd553aeb254cf3daf49
Pavlos Vinieratos <pvinis@gmail.com>
2023-01-20 12:10:42 +0200

    boom

---
+++ Formula/unicornleap.rb
@@ -0,0 +1,17 @@
+class Unicornleap < Formula
+  desc "Make unicorns leap across your screen!"
+  homepage "https://github.com/jgdavey/unicornleap"
+  version "github-main"
+  url "https://github.com/jgdavey/unicornleap/archive/refs/heads/main.zip"
+  sha256 "7ce11e8037c502e1348347b39381543b0908ae495b9c5fea9e33a137fbbf2bcf"
+
+  def install
+    system "make"
+    system "make", "images"
+    bin.install Dir["./build/unicornleap"]
+  end
+
+  test do
+    system "unicornleap", "-h"
+  end
+end
