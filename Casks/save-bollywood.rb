c746d41514968fa22eb0aa5c506d514e3aa459e8
Pavlos Vinieratos <pvinis@gmail.com>
2026-09-19 02:23:09 +0300

    Rename savebollywood to save-bollywood

--- Casks/save-bollywood.rb
+++ Casks/save-bollywood.rb
@@ -1,11 +1,11 @@
-cask "savebollywood" do
+cask "save-bollywood" do
   version "3.0"
   sha256 "f28833a6a36bd249c18de297eb673f58c16b6d0d6d6c4a0a24c33d3a293c69a4"
 
-  url "https://github.com/pvinis/savebollywood/releases/download/v#{version}/SaveBollywood-#{version}.zip"
+  url "https://github.com/pvinis/save-bollywood/releases/download/v#{version}/SaveBollywood-#{version}.zip"
   name "SaveBollywood"
   desc "Screen saver that plays your own video files"
-  homepage "https://github.com/pvinis/savebollywood"
+  homepage "https://github.com/pvinis/save-bollywood"
 
   depends_on macos: :tahoe
 
